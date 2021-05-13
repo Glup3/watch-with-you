6632d528d7b5db901c665d1ba143df9e70ee34cd
Phuc Tran <glup3.tran@gmail.com>
2021-05-13 15:02:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repository containing the Frontend and Backend for Watch-with-you
+
+watch-with-me v2 (watchtogether clone)
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Frontend runs</h1>
+        </div>
+    )
+}
+
+export default App
