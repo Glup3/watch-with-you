122fc1e2b7551bdfd631db1127bcd4f99f6c30b6
Phuc Tran <glup3.tran@gmail.com>
2021-05-13 22:58:33 +0200

    add socket io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { io } from 'socket.io-client'
 
 function App() {
+    useEffect(() => {
+        const socket = io('http://localhost:3010')
+        socket.emit('message', 'hi')
+    })
+
     return (
         <div className="App">
             <h1>Frontend runs</h1>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Server runs');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT || 3010;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+  console.log('Connected: ', socket.id);
+
+  socket.on('message', function (message: string) {
+    console.log(message);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
